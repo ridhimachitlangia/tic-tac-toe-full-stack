90b448d6e0e90a5cda909937b32560a410ea4678
ridhimachitlangia <ridhimachitlangia@gmail.com>
2026-06-11 16:23:00 +0530

    fix: adjust path mapping for render

--- tic-tac-toe/backend/server.js
+++ tic-tac-toe/backend/server.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
 const FILE = "./data/game.json";
