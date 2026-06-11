156f14f82f0295edba74e969e35e901677f5fb56
ridhimachitlangia <ridhimachitlangia@gmail.com>
2026-06-11 15:47:20 +0530

    fix: serve frontend files from backend

--- tic-tac-toe/backend/server.js
+++ tic-tac-toe/backend/server.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../frontend')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend', 'index.html'));
+});
 
 const FILE = "./data/game.json";
 
